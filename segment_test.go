77fa8c39695d49a13378e6afb8f2fb8d91594bea
Bill Mill <bill.mill@gmail.com>
2013-07-27 11:48:57 -0400

    adding a very simple test

---
+++ segment_test.go
@@ -0,0 +1,26 @@
+package segment
+
+import (
+	"testing"
+)
+
+func sliceEq(a, b []string) bool {
+    if len(a) != len(b) {
+        return false
+    }
+
+    for i := range a {
+        if a[i] != b[i] { return false }
+    }
+
+    return true
+}
+
+func TestSegment(t *testing.T) {
+	wordp := MakeWordProb("mobydick.txt")
+	seg := Segment("thereareshortpeopleeverywhere", wordp)
+
+	if !sliceEq(seg, []string{"there", "are", "short", "people", "everywhere"}) {
+		t.FailNow()
+	}
+}
